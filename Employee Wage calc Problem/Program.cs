a2cec76163079754db9dc996b41a6e4a1b15b6cb
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-27 20:41:27 +0530

    [Sai]Intial Commit Welcome Message!

---
+++ Employee Wage calc Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Wage_calc_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Problem");
+        }
+    }
+}
