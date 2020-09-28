1aba38e57c25d5e5bd5c8872117b79ecbd27dc24
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-28 10:48:06 +0530

    [Sai] Refactor. Static method

--- Employee Wage calc Problem/Program.cs
+++ Employee Wage calc Problem/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Employee_Wage_calc_Problem
 {
@@ -9,11 +8,12 @@ namespace Employee_Wage_calc_Problem
 
         const int IS_FULL_TIME = 1;
         const int IS_PART_TIME = 2;
-        
- 
+        const int TOTAL_WORKING_HOURS = 100;
+        const int TOTAL_WORKING_DAYS = 20;
+        const int WAGE_PER_HOUR = 20;
 
 
-        public static int CalculateWage(String comp_name, int wage_per_hour, int total_working_days, int total_working_hours)
+        public static int CalculateWage()
         {
             Random rand = new Random();
 
@@ -21,19 +21,14 @@ namespace Employee_Wage_calc_Problem
             //variable
 
             int numberOfHours = 0;
-            int workingDays = 1;
+            int workingDays = 0;
             int total_wage = 0;
 
-            // run till the number of hours or workingdays reached in respective company
 
-            while (numberOfHours <= total_working_hours && workingDays <= total_working_days)
+            while (numberOfHours <= TOTAL_WORKING_HOURS && workingDays <= TOTAL_WORKING_DAYS)
             {
-               
                 int type = rand.Next(0, 3);
                 int hours = 0;
-
-                // switch to type of Employee
-
                 switch (type)
                 {
                     case IS_FULL_TIME:
@@ -48,46 +43,23 @@ namespace Employee_Wage_calc_Problem
 
                 }
 
-                // add the hours to total hrs
-
                 numberOfHours += hours;
-
-                Console.Out.WriteLine(comp_name + " Day " + workingDays + " hours " + hours);
+                Console.Out.WriteLine("Day " + workingDays + " hours " + hours);
 
                 workingDays++;
 
+
             }
 
-            total_wage = numberOfHours * wage_per_hour;
+            total_wage = numberOfHours * WAGE_PER_HOUR;
 
             return total_wage;
         }
         static void Main(string[] args)
         {
 
-            int total_wage = 0;
-
-
-            Company Apple = new Company("Apple", 150, 20, 150);
-
-            Company Google = new Company("Google", 125, 22, 150);
-
-            IDictionary<Company, int> CompNames = new Dictionary< Company, int>();
-
-            total_wage = CalculateWage(Apple.comp_name, Apple.wage_per_hour,Apple.total_working_days,  Apple.total_working_hours);
- 
-            CompNames.Add(Apple, total_wage); //adding a key/value using the Add() method
-           
-            total_wage = CalculateWage(Google.comp_name, Google.wage_per_hour, Google.total_working_days, Google.total_working_hours);
-
-            CompNames.Add( Google ,total_wage);
-
-            foreach (KeyValuePair<Company, int> comp in CompNames)
-            {
-                Console.WriteLine(comp.Key.comp_name +"'s wage per month is : "+ comp.Value);
-            }
-
-            
+            int total_wage = CalculateWage();
+            Console.Out.WriteLine("Total wage of the month :" + total_wage);
 
         }
     }
